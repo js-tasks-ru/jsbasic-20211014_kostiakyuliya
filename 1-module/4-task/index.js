b6b0da14d25606777bf2dfa5b8aba6c17aa6c1a2
juliya-kostiak <60139572+juliya-kostiak@users.noreply.github.com>
2021-10-19 17:47:19 +0300

    1-module\3,4,5-tasks

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,14 @@
 function checkSpam(str) {
-  // ваш код...
+  let str1="1xBet";
+  let str2="XXX";
+  if(str.toLowerCase().includes(str1.toLowerCase()) || str.toLowerCase().includes(str2.toLowerCase())){
+    return true
+  }
+  else {
+    return false
+  }
 }
+
+
+//Напишите функцию checkSpam(str), возвращающую true, если str содержит '1xBet' или 'XXX', а иначе false.
+//Функция должна быть нечувствительна к регистру:
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,13 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if(str.length>maxlength){//усечение не требуется
+    let str1=str.slice(0,maxlength-1)+"…";
+    return str1;
+  }
+  else{
+    return str;
+  }
 }
+
+
+//Создайте функцию truncate(str, maxlength), которая проверяет длину строки str и, 
+//если она превосходит maxlength, заменяет конец str на "…", так, чтобы её длина стала равна maxlength.

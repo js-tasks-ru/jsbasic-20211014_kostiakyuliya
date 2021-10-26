1262b9ff2c21d44c44bc7f392c7e1a36a7ed8f09
juliya-kostiak <60139572+juliya-kostiak@users.noreply.github.com>
2021-10-26 21:34:36 +0300

    3-module\5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,25 @@
 function getMinMax(str) {
-  // ваш код...
+  let ArrOfNumbers = [];
+  let ArrOfStr = str.split(' ');
+  for(let key of ArrOfStr){
+    if (!isNaN(key)){
+      ArrOfNumbers.push(+key);
+    };
+  } 
+  let max  = ArrOfNumbers[0], min  = ArrOfNumbers[0];
+
+  for (let key of ArrOfNumbers) {
+    if (key > max) max = key;
+    if (key < min) min = key;
+  }
+
+  let result = {
+    min: null,
+    max: null
+  }
+
+  result.max=max;
+  result.min=min;
+
+  return result;
 }

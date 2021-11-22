aaeb068e9895379bfeb31625247235d7bf2d41b5
juliya-kostiak <60139572+juliya-kostiak@users.noreply.github.com>
2021-11-22 15:32:16 +0300

    5-module\3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,50 @@
 function initCarousel() {
-  // ваш код...
+
+  let numberOfClides=4;
+  let pointNow=0;
+
+  let carousel = document.querySelector(".carousel__inner");
+  let length = carousel.offsetWidth;
+
+  let butt_left = document.querySelector(".carousel__arrow_left");
+  let butt_right = document.querySelector(".carousel__arrow_right");
+
+  seeTheButton(pointNow);
+  butt_right.addEventListener("click",OnClickRight);
+  function OnClickRight(){
+    if(pointNow <= length*(numberOfClides - 1)){
+      carousel.style.transform = `translateX(-${pointNow+length}px)`;
+      pointNow = pointNow + length;
+      console.log(pointNow);
+      seeTheButton(pointNow);
+    }
+ 
+  }
+
+  butt_left.addEventListener("click",OnClickLeft);
+  function OnClickLeft(){
+    if(pointNow >= 0){
+      carousel.style.transform = `translateX(-${pointNow-length}px)`;
+      pointNow = pointNow - length;
+      console.log(pointNow);
+      seeTheButton(pointNow);
+    }
+  }
+
+  function seeTheButton(pointNow){
+    if(pointNow != 0){
+      butt_left.style.display = '';
+    }
+    else{
+      butt_left.style.display = 'none';
+    }
+
+    if (pointNow==length*(numberOfClides - 1)){
+      butt_right.style.display = 'none';
+    }
+    else{
+      butt_right.style.display = '';
+    }
+  }
+
 }
